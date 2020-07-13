fc8a560b4fea51434f53290748ce6e65cde58fe6
ParitoshGavali <paritosh191200@gmail.com>
2020-07-13 21:46:53 +0530

    First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿using System;
+namespace Image_Handelling
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnFwd = new System.Windows.Forms.Button();
+            this.labelPath = new System.Windows.Forms.Label();
+            this.Path = new System.Windows.Forms.TextBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.fileExporer = new System.Windows.Forms.ListView();
+            this.iconList = new System.Windows.Forms.ImageList(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnBack
+            // 
+            this.btnBack.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBack.Location = new System.Drawing.Point(48, 13);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(36, 23);
+            this.btnBack.TabIndex = 0;
+            this.btnBack.Text = "<<";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // btnFwd
+            // 
+            this.btnFwd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnFwd.Location = new System.Drawing.Point(100, 13);
+            this.btnFwd.Name = "btnFwd";
+            this.btnFwd.Size = new System.Drawing.Size(36, 23);
+            this.btnFwd.TabIndex = 1;
+            this.btnFwd.Text = ">>";
+            this.btnFwd.UseVisualStyleBackColor = true;
+            this.btnFwd.Click += new System.EventHandler(this.btnFwd_Click);
+            // 
+            // labelPath
+            // 
+            this.labelPath.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelPath.AutoSize = true;
+            this.labelPath.Location = new System.Drawing.Point(151, 18);
+            this.labelPath.Name = "labelPath";
+            this.labelPath.Size = new System.Drawing.Size(37, 17);
+            this.labelPath.TabIndex = 2;
+            this.labelPath.Text = "Path";
+            this.labelPath.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Path
+            // 
+            this.Path.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Path.Location = new System.Drawing.Point(190, 18);
+            this.Path.Name = "Path";
+            this.Path.Size = new System.Drawing.Size(462, 22);
+            this.Path.TabIndex = 3;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOpen.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnOpen.Location = new System.Drawing.Point(664, 18);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(67, 23);
+            this.btnOpen.TabIndex = 8;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox);
+            this.panel1.Location = new System.Drawing.Point(589, 65);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(240, 398);
+            this.panel1.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 162);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 8;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox.Location = new System.Drawing.Point(29, 15);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(182, 133);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.TabIndex = 7;
+            this.pictureBox.TabStop = false;
+            // 
+            // fileExporer
+            // 
+            this.fileExporer.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.fileExporer.HideSelection = false;
+            this.fileExporer.LargeImageList = this.iconList;
+            this.fileExporer.Location = new System.Drawing.Point(12, 53);
+            this.fileExporer.Name = "fileExporer";
+            this.fileExporer.Size = new System.Drawing.Size(570, 410);
+            this.fileExporer.SmallImageList = this.iconList;
+            this.fileExporer.TabIndex = 7;
+            this.fileExporer.UseCompatibleStateImageBehavior = false;
+            this.fileExporer.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.fileExporer_ItemSelectionChanged);
+            this.fileExporer.SelectedIndexChanged += new System.EventHandler(this.fileExporer_SelectedIndexChanged);
+            this.fileExporer.DoubleClick += new System.EventHandler(this.fileExporer_DoubleClick);
+            // 
+            // iconList
+            // 
+            this.iconList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("iconList.ImageStream")));
+            this.iconList.TransparentColor = System.Drawing.Color.Transparent;
+            this.iconList.Images.SetKeyName(0, "icons8-folder-48.png");
+            this.iconList.Images.SetKeyName(1, "file.png");
+            this.iconList.Images.SetKeyName(2, "video.jpeg");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(832, 478);
+            this.Controls.Add(this.fileExporer);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.Path);
+            this.Controls.Add(this.labelPath);
+            this.Controls.Add(this.btnFwd);
+            this.Controls.Add(this.btnBack);
+            this.MinimumSize = new System.Drawing.Size(850, 525);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnFwd;
+        private System.Windows.Forms.Label labelPath;
+        private System.Windows.Forms.TextBox Path;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListView fileExporer;
+        private System.Windows.Forms.ImageList iconList;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ win_xplorer.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Collections.Specialized;
+using System.IO;
+
+
+
+namespace Image_Handelling
+{
+    public partial class Form1 : Form
+    {
+        private string[] filePathPrev = new string[100];
+        private int prevIndex = 0;
+        private string filePath = "F:";
+        private bool isFile = false;
+        private bool isBack = false;
+        private string currentlySelectedItemName = "";
+        private int numbericons = 3;
+        ImageList imgs = new ImageList();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*public Bitmap ResizeBitmap(Bitmap b, int nWidth, int nHeight)
+        {
+            Bitmap result = new Bitmap(nWidth, nHeight);
+
+            using (Graphics g = Graphics.FromImage((Image)result))
+                //g.DrawImage(b,)
+                g.DrawImage(b, 0, 0, nWidth, nHeight);
+            return result;
+        }*/
+
+        public Bitmap ResizeBitmap(Bitmap b, int nWidth, int nHeight)
+        {
+            int MaxSide = nWidth > nHeight ? nWidth : nHeight;
+            Bitmap result = new Bitmap(MaxSide, MaxSide);
+            using (Graphics g = Graphics.FromImage((Image)result)) {
+                g.Clear(Color.Transparent);
+                int xOffset = (MaxSide - nWidth) / 2;
+                int yOffset = (MaxSide - nHeight) / 2;
+                g.DrawImage(b, new Point(xOffset, yOffset));
+            }
+            return result;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Path.Text = filePath;
+            filePathPrev[prevIndex] = filePath; 
+            loadFilesAndDirectories();
+            //iconList.ImageSize = new Size(64,64);
+            //for image icon:
+            //fileExporer.View = View.Details;
+
+            //list of image icons
+        }
+
+        public void loadButtonAction()
+        {
+            filePath = Path.Text;
+            loadFilesAndDirectories();
+            isFile = false;
+        }
+
+
+        public void loadFilesAndDirectories()
+        {
+            DirectoryInfo filelist;
+            string tempFilePath = "";
+            FileAttributes fileAttr;
+            try
+            {
+                if (isFile)
+                {
+                    tempFilePath = filePath + "/" + currentlySelectedItemName;
+                    FileInfo fileDetails = new FileInfo(tempFilePath);
+                    //Path.Text = fileDetails
+                    fileAttr = File.GetAttributes(tempFilePath);
+                    Process.Start(tempFilePath);
+                }
+                else
+                {
+                    fileAttr = File.GetAttributes(filePath);
+                }
+
+                if((fileAttr & FileAttributes.Directory) == FileAttributes.Directory)
+                {
+                    if (!isBack)
+                    {
+                        filePathPrev[++prevIndex] = filePath;
+                    }
+                    else
+                    {
+                        isBack = false;
+                    }
+                    filelist = new DirectoryInfo(filePath);
+                    FileInfo[] files = filelist.GetFiles();
+                    DirectoryInfo[] dirs = filelist.GetDirectories();
+                    string fileExtension = "";
+
+                    fileExporer.Items.Clear();
+                    for(int i = numbericons; i < iconList.Images.Count; i++)
+                    {
+                        iconList.Images.RemoveAt(i);
+                    }
+
+                    //fileExporer.SmallImageList = imgs;
+                    for (int i = 0; i < files.Length; i++)
+                    {
+                        fileExtension = files[i].Extension.ToUpper();
+                        GC.Collect();
+                        switch (fileExtension)
+                        {
+                            case ".PNG":
+                            case ".JPG":
+                            case ".JPEG":
+                                //imgs.Images.Add(Image.FromFile(files[i].FullName));
+                                //MessageBox.Show(imgs.Images.Count);
+                                Bitmap image = new Bitmap(files[i].FullName);
+                                //MessageBox.Show(image.Width.ToString() + "x" + image.Height.ToString());
+                                image = ResizeBitmap(image, image.Width, image.Height);
+                                iconList.Images.Add(image);
+                                fileExporer.Items.Add(files[i].Name,iconList.Images.Count-1);
+                                //iconList.Images.RemoveAt(iconList.Images.Count - 1);
+                                break;
+                            case ".MP4":
+                            case ".AVI":
+                                fileExporer.Items.Add(files[i].Name,2);
+                                break;
+                            default:
+                                fileExporer.Items.Add(files[i].Name,1);
+                                break;
+                        }
+                        //fileExporer.Items.Add(files[i].Name);
+                    }
+                    GC.Collect();
+                    for (int i = 0; i < dirs.Length; i++)
+                    {
+                        fileExporer.Items.Add(dirs[i].Name,0);
+                    }
+                }
+                else
+                {
+                    //imageDesp.Text = this.currentlySelectedItemName;
+                }
+
+            }
+            catch(Exception e)
+            {
+
+            }
+        }
+
+        private void btnFwd_Click(object sender, EventArgs e)
+        {
+            loadButtonAction();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog() { Description = "Select your path" })
+            {
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    //fileExplorer.Url = new Uri(fbd.SelectedPath);
+                }
+            }
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            using(FolderBrowserDialog fbd = new FolderBrowserDialog() { Description="Select your path"})
+            {
+                if(fbd.ShowDialog()== DialogResult.OK)
+                {
+                    //fileExplorer.Url = new Uri(fbd.SelectedPath);
+                    //Path.Text = fileExplorer.Url.ToString();
+                }
+            }
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            /*if(fileExplorer.CanGoBack == true)
+            {
+                fileExplorer.GoBack();
+            }*/
+            prevIndex--;
+            if(prevIndex >= 0)
+            {
+                Path.Text = filePathPrev[prevIndex];
+                isBack = true;
+                loadButtonAction();
+            }
+            else
+            {
+                filePathPrev = new string[100];
+                prevIndex = 0;
+                filePathPrev[prevIndex] = Path.Text;
+            }
+            
+        }
+
+        
+
+        
+
+        private void fileExplorer_Navigating(object sender, WebBrowserNavigatingEventArgs e)
+        {
+            
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fileExporer_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
+        {
+            currentlySelectedItemName = e.Item.Text;
+
+            FileAttributes fileAttr = File.GetAttributes(filePath + "/" + currentlySelectedItemName);
+            if ((fileAttr & FileAttributes.Directory) == FileAttributes.Directory)
+            {
+                isFile = false;
+                Path.Text = filePath + "/" + currentlySelectedItemName;
+            }
+            else
+            {
+                isFile = true;
+            }
+        }
+
+        private void fileExporer_DoubleClick(object sender, EventArgs e)
+        {
+            loadButtonAction();
+        }
+
+        public bool isImage(string s)
+        {
+            if(s.EndsWith(".jpg") || s.EndsWith(".png") || s.EndsWith(".jpeg") || s.EndsWith(".bmp"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        
+
+        private void fileExporer_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //imageDesp.Text = currentlySelectedItemName;
+            //var item = fileExporer.SelectedItems[0];
+            //imageDesp.Text = item.Text + "\n" + item.Tag;
+            try
+            {
+                //var selectedobj = fileExporer.SelectedItems[0].ToString();
+                if(!string.IsNullOrEmpty(currentlySelectedItemName) && !string.IsNullOrEmpty(filePath))
+                {
+                    var fullpath = filePath + "/" + currentlySelectedItemName;
+                    FileInfo file = new FileInfo(fullpath);
+                    label1.Text = "Name : " + file.Name + "\nCreated on : " + file.CreationTime.ToShortDateString() + "\nSize : " + Math.Round(file.Length/(1024.0*1024.0),2) + " MB";
+                    if (isImage(currentlySelectedItemName))
+                    {
+                        pictureBox.Image = Image.FromFile(fullpath);
+                    }
+                    else
+                    {
+                        pictureBox.Image = null;
+                    }
+                    
+                }
+            }
+            catch(Exception)
+            {
+
+            }
+            GC.Collect();
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
